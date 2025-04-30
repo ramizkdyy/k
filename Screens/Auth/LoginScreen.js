3df035f22dea7c4dcbe02c2ccbd68516aad514d4
ramizkdyy <ramiz.kadayifci@bilgiedu.net>
2025-05-01 00:55:30 +0300

    navigation

---
+++ Screens/Auth/LoginScreen.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>LoginScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
